fe254c40654303b830b5b46821ab75ed6f263e68
Nahuel Cosme <ncdv.2003@gmail.com>
2024-04-04 09:22:23 +0100

    Vistas estáticas de login, register, movie description y search result añadidas.Estilos globales añadidos. Fotos de perfil y portada de Avatar agregados

--- sprint3-MOVIEIT/src/app/app.component.ts
+++ sprint3-MOVIEIT/src/app/app.component.ts
@@ -1,10 +1,14 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import {LoginComponent} from "./login/login.component";
+import {RegisterComponent} from "./register/register.component";
+import {MovieDescriptionComponent} from "./movie-description/movie-description.component";
+import {SearchResultPageComponent} from "./search-result-page/search-result-page.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, LoginComponent, RegisterComponent, MovieDescriptionComponent, SearchResultPageComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
