95880fe682db8d85ec0bd5b6c04d6fd35ff01e56
Nahuel Cosme <ncdv.2003@gmail.com>
2024-04-08 11:44:47 +0100

    Enrutamiento a páginas Home, Login y Register

--- sprint3-MOVIEIT/src/app/app.routes.ts
+++ sprint3-MOVIEIT/src/app/app.routes.ts
@@ -1,3 +1,16 @@
-import { Routes } from '@angular/router';
+import {RouterModule, Routes} from '@angular/router';
+import {HomeComponent} from "./Views/home/home.component";
+import {LoginComponent} from "./login/login.component";
+import {RegisterComponent} from "./register/register.component";
+import {NgModule} from "@angular/core";
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {path: 'home', component: HomeComponent},
+  {path: '', component: HomeComponent},
+  {path: 'login', component: LoginComponent},
+  {path: 'register', component: RegisterComponent}];
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule {}
--- sprint3-MOVIEIT/src/app/login/login.component.ts
+++ sprint3-MOVIEIT/src/app/login/login.component.ts
@@ -1,12 +1,19 @@
 import { Component } from '@angular/core';
+import {Router, RouterLink} from "@angular/router";
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [],
+  imports: [
+    RouterLink
+  ],
   templateUrl: './login.component.html',
   styleUrl: './login.component.css'
 })
 export class LoginComponent {
-
+constructor(private router: Router) {
+}
+navigateToRegister() {
+  this.router.navigate(['/register'])
+}
 }
