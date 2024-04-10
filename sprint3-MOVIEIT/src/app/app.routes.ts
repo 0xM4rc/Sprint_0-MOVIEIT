5080b45e1a97e973e36f6ba6de775bf9a1b53e20
Nahuel Cosme <ncdv.2003@gmail.com>
2024-04-10 19:31:34 +0100

    Añadida nueva vista NotFound. Se carga cuando se introduce una url errónea

--- sprint3-MOVIEIT/src/app/app.routes.ts
+++ sprint3-MOVIEIT/src/app/app.routes.ts
@@ -4,13 +4,15 @@ import {LoginComponent} from "./login/login.component";
 import {RegisterComponent} from "./register/register.component";
 import {NgModule} from "@angular/core";
 import {MovieDescriptionComponent} from "./movie-description/movie-description.component";
+import {NotfoundComponent} from "./views/notfound/notfound.component";
 
 export const routes: Routes = [
-  {path: 'home', component: HomeComponent},
   {path: '', component: HomeComponent},
+  {path: 'home', component: HomeComponent},
   {path: 'login', component: LoginComponent},
   {path: 'register', component: RegisterComponent},
-  {path: 'movie-description', component: MovieDescriptionComponent}];
+  {path: 'movie-description', component: MovieDescriptionComponent},
+  {path: '**', component: NotfoundComponent}];
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
