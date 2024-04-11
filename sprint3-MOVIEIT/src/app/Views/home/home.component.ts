6de84b40401969c9d54583568a3e73f20d9e7175
hunte <hunterofseries13@gmail.com>
2024-04-11 09:37:06 +0100

    Profile header hecho,alert:home cambiado

--- sprint3-MOVIEIT/src/app/Views/home/home.component.ts
+++ sprint3-MOVIEIT/src/app/Views/home/home.component.ts
@@ -1,13 +1,14 @@
 import { Component } from '@angular/core';
 import { HeaderComponent } from "../../components/header/header.component";
 import { FooterComponent } from "../../components/footer/footer.component";
+import { ProfileHeaderComponent } from "../../components/profile-header/profile-header.component";
 
 @Component({
     selector: 'app-home',
     standalone: true,
     templateUrl: './home.component.html',
     styleUrl: './home.component.css',
-    imports: [HeaderComponent, FooterComponent]
+    imports: [HeaderComponent, FooterComponent,ProfileHeaderComponent]
 })
 export class HomeComponent {
 
