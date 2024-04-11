a253f0e347c61982ee0de3bf0a9f117fb2efb5de
hunte <hunterofseries13@gmail.com>
2024-04-11 09:50:35 +0100

    Añadido score

--- sprint3-MOVIEIT/src/app/Views/profile/profile.component.ts
+++ sprint3-MOVIEIT/src/app/Views/profile/profile.component.ts
@@ -2,15 +2,17 @@ import { Component } from '@angular/core';
 import {FooterComponent} from "../../components/footer/footer.component";
 import {HeaderComponent} from "../../components/header/header.component";
 import {ProfileHeaderComponent} from "../../components/profile-header/profile-header.component";
+import {ScoreComponent} from "../../components/score/score.component";
 
 @Component({
   selector: 'app-profile',
   standalone: true,
-    imports: [
-        FooterComponent,
-        HeaderComponent,
-        ProfileHeaderComponent
-    ],
+  imports: [
+    FooterComponent,
+    HeaderComponent,
+    ProfileHeaderComponent,
+    ScoreComponent,
+  ],
   templateUrl: './profile.component.html',
   styleUrl: './profile.component.css'
 })
