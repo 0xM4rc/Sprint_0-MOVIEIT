b97f59c217331ee9a7c9fea2ad42b4984335d904
Nahuel Cosme <ncdv.2003@gmail.com>
2024-04-15 12:03:47 +0100

    Vistas metidas en carpeta views. Fichas de películas añadidas a películas relacionadas de Movie description

--- sprint3-movieit/src/app/app.routes.ts
+++ sprint3-movieit/src/app/app.routes.ts
@@ -1,9 +1,9 @@
 import {RouterModule, Routes} from '@angular/router';
 import {HomeComponent} from "./views/home/home.component";
-import {LoginComponent} from "./login/login.component";
-import {RegisterComponent} from "./register/register.component";
+import {LoginComponent} from "./views/login/login.component";
+import {RegisterComponent} from "./views/register/register.component";
 import {NgModule} from "@angular/core";
-import {MovieDescriptionComponent} from "./movie-description/movie-description.component";
+import {MovieDescriptionComponent} from "./views/movie-description/movie-description.component";
 import {NotfoundComponent} from "./views/notfound/notfound.component";
 
 export const routes: Routes = [
