65aeee6a03948c393aad1595c1c6797ce14ea5b1
Nahuel Cosme <ncdv.2003@gmail.com>
2024-04-19 20:46:59 +0100

    Typescript de home modificado

--- sprint3-movieit/src/app/views/home/home.component.ts
+++ sprint3-movieit/src/app/views/home/home.component.ts
@@ -6,6 +6,7 @@ import {NgForOf, NgIf} from "@angular/common";
 import {Film} from "../../models/film.model";
 import {FilmService} from "../../services/film.service";
 import {RouterLink} from "@angular/router";
+import {MovieDescriptionComponent} from "../movie-description/movie-description.component";
 
 @Component({
     selector: 'app-home',
@@ -15,21 +16,17 @@ import {RouterLink} from "@angular/router";
   imports: [HeaderComponent, FooterComponent, HighlightedFilmCardComponent, NgForOf, NgIf, RouterLink]
 })
 export class HomeComponent implements OnInit{
-  items= [{ id: "1", fileUrl: '../../../assets/images/films_covers/avatar.jpg', time: 30},
-    { id: "2", fileUrl: '../../../assets/images/films_profile_covers/AliciaInWonderwall.webp', time: 90},
-    { id: "3", fileUrl: '../../../assets/images/films_covers/breaking_bad.jpg', time: 10},
-    { id: "4", fileUrl: '../../../assets/images/films_covers/matrix.jpg', time: 80},
-    { id: "5", fileUrl: '../../../assets/images/films_covers/the_dark_knight.jpg', time: 30},
-    { id: "6", fileUrl: '../../../assets/images/films_covers/the_godfather.jpg', time: 30},
-    { id: "7", fileUrl: '../../../assets/images/films_profile_covers/Ghostbusters.jpg', time: 30},
-    { id: "8", fileUrl: '../../../assets/images/films_profile_covers/johnWick.jpg', time: 30}]
-  ;
   films: Film[] = [];
+  selectedFilm: Film | null = null;
 
   constructor(private filmService: FilmService) {
   }
 
   ngOnInit() {
     this.filmService.getFilms().subscribe(films => this.films = films);
+  }
+
+  selectFilm(film: Film) {
+    this.filmService.setFilm(film);
   }
 }
