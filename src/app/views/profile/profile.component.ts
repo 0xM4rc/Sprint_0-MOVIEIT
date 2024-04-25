79e207f141b0c36004f0d767c5d3afef6bfb4ac0
hunte <hunterofseries13@gmail.com>
2024-04-25 09:39:40 +0100

    profile añadido

---
+++ src/app/views/profile/profile.component.ts
@@ -0,0 +1,13 @@
+import {Component, NgModule} from '@angular/core';
+import { ProfileHeaderComponent } from '../../components/profile-header/profile-header.component';
+
+@Component({
+  selector: 'app-profile',
+  templateUrl: './profile.component.html',
+  styleUrl: './profile.component.css',
+})
+
+
+export class ProfileComponent {
+
+}
