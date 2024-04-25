cfa1f02fc1635348a8712b19f72c228f67f5ed36
hunte <hunterofseries13@gmail.com>
2024-04-25 09:46:07 +0100

    edit-profile añadido

---
+++ src/app/views/edit-profile/edit-profile.module.ts
@@ -0,0 +1,12 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { ProfileHeaderComponent } from '../../components/profile-header/profile-header.component';
+
+
+@NgModule({
+  declarations: [ProfileHeaderComponent],
+  imports: [
+    CommonModule
+  ]
+})
+export class EditProfileModule { }
