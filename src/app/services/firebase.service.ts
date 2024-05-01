c20e1590e591f1406e3b2564a07fdb7748e912a3
0xM4rc <marcos.sanchez2003@proton.me>
2024-05-01 13:05:57 +0100

    Carga de caratulas en el home desde el firebase, cambio de nombres de components  y el titulo de la página

---
+++ src/app/services/firebase.service.ts
@@ -0,0 +1,25 @@
+import { Injectable } from '@angular/core';
+import { AngularFirestore } from '@angular/fire/compat/firestore';
+import { Observable, catchError, map, of } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class FirebaseService {
+
+  constructor(private firestore: AngularFirestore) { }
+  
+   getCoverUrlById(coverId: string): Observable<string> {
+    // Método para obtener la URL de una portada por su ID
+    return this.firestore.doc(`portadas/${coverId}`).snapshotChanges().pipe(
+      map(action => {
+        const data = action.payload.data() as any;
+        return data ? data.url : null;
+      }),
+      catchError(err => {
+        console.error(err);
+        return of('');
+      })
+    );
+  }
+}
--- src/app/views/home/home.component.ts
+++ src/app/views/home/home.component.ts
@@ -1,19 +1,33 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FirebaseService } from '../../services/firebase.service';
+import Cover from '../../interfaces/cover';
+
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
-export class HomeComponent {
-  items= [{ id: 1, fileUrl: '../../../assets/images/films_covers/avatar.jpg', time: 30},
-    { id: 2, fileUrl: '../../../assets/images/films_profile_covers/AliciaInWonderwall.webp', time: 90},
-    { id: 3, fileUrl: '../../../assets/images/films_covers/breaking_bad.jpg', time: 10},
-    { id: 4, fileUrl: '../../../assets/images/films_covers/matrix.jpg', time: 80},
-    { id: 5, fileUrl: '../../../assets/images/films_covers/the_dark_knight.jpg', time: 30},
-    { id: 6, fileUrl: '../../../assets/images/films_covers/the_godfather.jpg', time: 30},
-    { id: 7, fileUrl: '../../../assets/images/films_profile_covers/Ghostbusters.jpg', time: 30},
-    { id: 8, fileUrl: '../../../assets/images/films_profile_covers/johnWick.jpg', time: 30}]
-  ;
 
+export class HomeComponent implements OnInit {
+  items: Cover[] = [];
+
+  constructor(private firebaseService: FirebaseService) {}
+
+  ngOnInit() {
+    this.loadUrls();
+  }
+
+  loadUrls() {
+    const ids = [1, 2, 1, 1, 1, 1, 2, 1];
+    ids.forEach(id => {
+      this.firebaseService.getCoverUrlById(id.toString()).subscribe(url => {
+        this.items.push({
+          id: id,
+          fileUrl: url,
+          time: 30
+        });
+      });
+    });
+  }
 }
