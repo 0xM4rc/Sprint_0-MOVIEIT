69a1a2a7a0b2490076d5adca1721c44b1885c822
hunte <hunterofseries13@gmail.com>
2024-05-04 22:45:30 +0100

    Peliculas añadidas al profile

--- src/app/views/profile/profile.component.ts
+++ src/app/views/profile/profile.component.ts
@@ -1,4 +1,7 @@
-import { Component } from '@angular/core';
+import { Component,OnInit } from '@angular/core';
+import Cover from "../../interfaces/cover";
+import {FirebaseService} from "../../services/firebase.service";
+
 
 @Component({
   selector: 'app-profile',
@@ -6,13 +9,45 @@ import { Component } from '@angular/core';
   styleUrl: './profile.component.css'
 })
 export class ProfileComponent {
-  items= [{ id: 1, fileUrl: '../../../assets/images/films_profile_covers/TronLegacy.jpg', time: 30},
-    { id: 2, fileUrl: '../../../assets/images/films_profile_covers/AliciaInWonderwall.webp', time: 30},
-    { id: 3, fileUrl: '../../../assets/images/films_profile_covers/AboutTime.jpg', time: 10},
-    { id: 4, fileUrl: '../../../assets/images/films_profile_covers/TheIntern.jpg', time: 80},
-    { id: 5, fileUrl: '../../../assets/images/films_profile_covers/OperacionUncle.jpg', time: 30},
-    { id: 6, fileUrl: '../../../assets/images/films_profile_covers/Avatar2.jpeg', time: 30},
-    { id: 7, fileUrl: '../../../assets/images/films_profile_covers/Ghostbusters.jpg', time: 30},
-    { id: 8, fileUrl: '../../../assets/images/films_profile_covers/johnWick.jpg', time: 30}]
-  ;
+  favorites: Cover[] = [];
+  pending: Cover[] = [];
+  watched: Cover[] = [];
+
+  constructor(private firebaseService: FirebaseService) {}
+
+  ngOnInit() {
+    this.loadUrls()
+  }
+
+  loadUrls() {
+    const ids = Array.from({length: 17}, (_, i) => i + 6);
+    let num :number = 1;
+    ids.forEach(id => {
+      this.firebaseService.getCoverUrlById(id.toString()).subscribe(url => {
+        if(num == 1){
+          num++;
+          this.favorites.push({
+            id: id,
+            fileUrl: url,
+            time: 30
+          });
+        }else if(num == 2){
+          num++;
+          this.pending.push({
+            id: id,
+            fileUrl: url,
+            time: 30
+          });
+        }else{
+          num = 1;
+          this.watched.push({
+            id: id,
+            fileUrl: url,
+            time: 30
+          });
+        }
+
+      });
+    });
+  }
 }
