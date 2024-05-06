090299776b89a78e72c8d5b8765c555d93ca0a4f
Nahuel Cosme <ncdv.2003@gmail.com>
2024-05-06 12:17:22 +0100

    Inicialización del repo para el sprint 4

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'movieit',
+  webDir: 'www'
+};
+
+export default config;
